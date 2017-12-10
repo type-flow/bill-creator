e72a1f329c34e702381112f8c29f89761601b4ae
Lukas Holzer <office@lukasholzer.com>
2017-12-10 12:05:23 +0100

    add print component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { PrintComponent } from './pages/print/print.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrintComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/pages/print/print.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './print.component.html',
+  styleUrls: ['./print.component.scss']
+})
+export class PrintComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
