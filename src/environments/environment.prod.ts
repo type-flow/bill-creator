6ee69ded3fd3e6884a98e6944067386906baeeff
Lukas Holzer <office@lukasholzer.com>
2017-12-17 11:51:26 +0100

    update environments with firebase cred.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBxmRZGtmRHdjQYswQ_U_8wZ5kRyPSxFm8",
+    authDomain: "customer-management-a8d59.firebaseapp.com",
+    databaseURL: "https://customer-management-a8d59.firebaseio.com",
+    projectId: "customer-management-a8d59",
+    storageBucket: "customer-management-a8d59.appspot.com",
+    messagingSenderId: "650588897481"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBxmRZGtmRHdjQYswQ_U_8wZ5kRyPSxFm8",
+    authDomain: "customer-management-a8d59.firebaseapp.com",
+    databaseURL: "https://customer-management-a8d59.firebaseio.com",
+    projectId: "customer-management-a8d59",
+    storageBucket: "customer-management-a8d59.appspot.com",
+    messagingSenderId: "650588897481"
+  }
 };
